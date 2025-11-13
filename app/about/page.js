7b8d10e8e681ae6675c472cd11489e7f4c227a69
mohilow12 <mohilow12@gmail.com>
2025-11-13 01:07:18 -0800

    Update page.js
    minor tweaks

--- app/about/page.js
+++ app/about/page.js
@@ -222,7 +222,7 @@ export default function AboutPage() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-8 justify-items-center">
             {coreValues.map((value, index) => (
               <div
                 key={index}
